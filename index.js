28ee57c7c68f9191b8ae11e710e87ebfcb84b6f6
Vidar Eldøy <vidar@eldoy.com>
2022-12-09 09:21:58 +0100

    Add all services

--- index.js
+++ index.js
@@ -2,37 +2,55 @@ const path = require('path')
 const soap = require('soap')
 const xml2js = require('xml2js')
 const parser = new xml2js.Parser()
+const services = require('./lib/services.js')
 
-const url = 'https://grunndata.brreg.no/grunndata/ErFr.wsdl'
-const config = require(path.join(process.cwd(), 'brreg.json'))
+function brregpro(config = {}) {
+  function fetch(uri, query) {
+    const [name, endpoint] = uri.split('/')
+    console.log({ name, endpoint })
+    const { url } = services[name]
 
-const query = { ...config, orgnr: '992616091' }
+    console.log({ url })
 
-function fetch(url, query) {
-  return new Promise(function (resolve, reject) {
-    soap.createClient(url, {}, function (err, client) {
-      if (err) {
-        return reject(err)
-      }
-      client.hentBasisdataMini(query, function (err, data) {
+    query = { ...config, ...query }
+
+    console.log(query)
+
+    return new Promise(function (resolve, reject) {
+      soap.createClient(url, {}, function (err, client) {
         if (err) {
           return reject(err)
         }
-        resolve(data.return)
+        const desc = client.describe()
+        console.log(JSON.stringify(desc, null, 2))
+        client[endpoint](query, function (err, data) {
+          if (err) {
+            return reject(err)
+          }
+          console.log(data.return)
+          parser
+            .parseStringPromise(data.return)
+            .then(function (json) {
+              resolve(json)
+            })
+            .catch(function (err) {
+              reject(err)
+            })
+        })
       })
     })
+  }
+
+  return { fetch }
+}
+
+async function run() {
+  const config = require(path.join(process.cwd(), 'brreg.json'))
+  const brreg = brregpro(config)
+  const result = await brreg.fetch('enhet/hentBasisdataMini', {
+    orgnr: '992616091'
   })
+  console.log(JSON.stringify(result, null, 2))
 }
 
-async function brregpro() {
-  let data = await fetch(url, query)
-
-  console.log(data)
-
-  console.log(data)
-
-  const json = await parser.parseStringPromise(data)
-  console.log(json)
-}
-
-brregpro()
+run()
